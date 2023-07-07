114539d8079fd710a9ece52913044982523da246
akarounis <karounisa@unisystems.gr>
2023-07-07 19:11:15 +0300

    Finished user related services

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -13,19 +13,16 @@ namespace Inveasy.Services
             _context = context;
         }
 
-//----- User services ----- //
-
+        #region User related services
         // Method to retrieve user with given username
         public async Task<User> GetUser(string username) => await _context.User.FirstOrDefaultAsync(d => d.Username == username);               
         
         // Method to add a new user
         public async Task<bool> AddUser(User user)
         {
-            if (GetUser(user.Username) != null)
-            {                
+            if (GetUser(user.Username) != null)                
                 return false; // User already exists                                
-            }
-
+            
             try
             {
                 // Adding given user
@@ -141,8 +138,129 @@ namespace Inveasy.Services
             {
                 return false;
             }
+        }
 
-        }        
+        // Method that removes role with given role id from user with given username
+        public async Task<bool> RemoveRoleFromUser(int roleId, string username)
+        {
+            // Get user
+            User user = await GetUser(username);
+
+            // If user doesn't exist, do nothing
+            if (user == null)
+                return false;
+
+            // If user already does not have given role, return true
+            if (!user.Roles.Select(d => d.Id == roleId).Any())
+                return true;
+
+            try
+            {
+                Role desiredRole = await _context.Role.FirstOrDefaultAsync(d => d.Id == roleId);
+                if (desiredRole == null) return false; // Role with given id doesn't exist
+
+                user.Roles.Remove(desiredRole);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        // Method that removes role with given role name to user with given username
+        public async Task<bool> RemoveRoleFromUser(string roleName, string username)
+        {
+            // Get user
+            User user = await GetUser(username);
+
+            // If user doesn't exist, do nothing
+            if (user == null)
+                return false;
+
+            // If user already has given role, return true
+            if (!user.Roles.Select(d => d.Name == roleName).Any())
+                return true;
+
+            try
+            {
+                Role desiredRole = await _context.Role.FirstOrDefaultAsync(d => d.Name == roleName);
+                if (desiredRole == null) return false; // Role with given name doesn't exist
+
+                user.Roles.Remove(desiredRole);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        // Method that gets donations made by a user with given username
+        public async Task<List<Donation>> GetUserDonations(string username)
+        {
+            // Get user
+            var user = await GetUser(username);
+
+            // If user doesn't exist, do nothing
+            if (user == null)
+                return null;
+
+            try
+            {
+                return await _context.Donation.Where(d => d.User.Username == username).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<View>> GetUserViews(string username)
+        {
+            // Get user
+            var user = await GetUser(username);
+
+            // If user doesn't exist, do nothing
+            if (user == null)
+                return null;
+
+            try
+            {
+                return await _context.View.Where(d => d.User.Username == username).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<Project>> GetUserProjects(string username)
+        {
+            // Get user
+            var user = await GetUser(username);
+
+            // If user doesn't exist, do nothing
+            if (user == null)
+                return null;
+
+            // If user doesn't have role of Project creator, do nothing
+            if (!user.Roles.Select(d => d.Id == 1).Any())
+                return null;
+
+            try
+            {
+                return await _context.Project.Where(d => d.User.Username == username).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        #endregion
+
 
     }
 
