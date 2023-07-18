929d8ccc7617a4864a8baa374e179a8ac4792ca0
Cleo Beka <cleobeka@hotmail.com>
2023-07-19 01:30:56 +0300

    page templates

---
+++ Controllers/UserFundedController.cs
@@ -0,0 +1,14 @@
+﻿using Inveasy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Inveasy.Controllers
+{
+    public class UserFundedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
