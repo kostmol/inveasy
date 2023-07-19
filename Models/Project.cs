06e85a722f778163731f29d2adf7065fa534d56d
akarounis <karounisa@unisystems.gr>
2023-07-20 00:56:51 +0300

    Initialized all lists in project

--- Models/Project.cs
+++ Models/Project.cs
@@ -11,11 +11,11 @@ namespace Inveasy.Models
         public double? FundGoal { get; set; }
         public double? TrendingScore { get; set; }
         public virtual User? User { get; set; }
-        public virtual List<Category>? Categories { get; set; }
-        public virtual List<View>? Views { get; set; }
-        public virtual List<Comment>? Comments { get; set; }
-        public virtual List<Donation>? Donations { get; set; }
-        public virtual List<RewardTier>? RewardsTier { get; set; }
+        public virtual List<Category> Categories { get; set; } = new List<Category>();
+        public virtual List<View> Views { get; set; } = new List<View>();
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+        public virtual List<Donation> Donations { get; set; } = new List<Donation>();
+        public virtual List<RewardTier> RewardsTier { get; set; } = new List<RewardTier>();
         public virtual DateTime CreatedDate { set; get; }
         public ICollection<Image> Images { get; set; } = new List<Image>();
     }
