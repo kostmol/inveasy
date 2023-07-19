da53bfefb3b615a2e904d46f8788a9943cbd9e7a
kostmol <kostmol@gmail.com>
2023-07-20 01:00:22 +0300

    nav links

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,14 +1,40 @@
-﻿using Inveasy.Models;
+﻿using System.Text.Json;
+using Inveasy.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Inveasy.Services.UserServices;
 
 namespace Inveasy.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IUserService _userService;
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+            var authUser = await _userService.GetUserAsync(user.Username);
+            if (authUser != null)
+            {
+                HttpContext.Session.SetString("User", JsonSerializer.Serialize(authUser));
+                return RedirectToAction("Index", "Home");
+            }
+            
+            ModelState.AddModelError("", "Invalid login attempt");
+            return RedirectToAction("Index");
         }
     }
 }
