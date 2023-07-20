bdddd74f035ca3846742777895a5d0ff22089727
Cleo Beka <cleobeka@hotmail.com>
2023-07-20 21:05:26 +0300

    project comments & profile page

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Inveasy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Inveasy.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
