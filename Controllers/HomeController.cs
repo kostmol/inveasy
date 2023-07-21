a309112b9e6d357ee372a319cd922e0b1eb12219
kostmol <kostmol@gmail.com>
2023-07-21 10:25:30 +0300

    home profile

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,38 @@
 ﻿using Inveasy.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Inveasy.Services.ProjectServices;
+using Inveasy.Services.UserServices;
 
 namespace Inveasy.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProjectService _projectService;
+        private readonly IUserService _userService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProjectService projectService, IUserService userService)
         {
             _logger = logger;
+            _projectService = projectService;
+            _userService = userService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var projects = await _projectService.GetProjectsAsync();
+            var users = await _userService.GetUsersAsync();
+            
+            // users.Count(u => u.Roles in ())
+            var totalMoney = projects.Sum(p => p.FundAmount);
+            var pFunded = projects.Count(p => p.FundAmount > 0);
+            var backers = users.Count(u => u.Roles.Any(r => r.Name == "Backer"));
+            
+            ViewData["totalMoney"] = totalMoney;
+            ViewData["pFunded"] = pFunded;
+            ViewData["backers"] = backers;
+            
             return View();
         }
         public IActionResult Project()
